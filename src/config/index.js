a2e793da0ec5c9022fe7414e6b735fc075aef2ee
liuweimin <liuweiminer@hotmail.com>
2020-03-17 16:18:20 +0800

    use hexo

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-const configInfo = require('./config.json')
-const avatar = /^http?/gi.test(configInfo.avatar)
-  ? configInfo.avatar
-  : require(configInfo.avatar + '')
-export default {
-  ...configInfo,
-  avatar: avatar
-}
--- src/containers/TagList/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const GET_TAG_INFO = 'get_tag_info'
-export const GET_POST_INFO_BY_TAG = 'get_post_list_by_tag'
-export const SET_TAG_NAME = 'set_tag_name'
